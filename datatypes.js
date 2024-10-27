4a8e7cfd82d45bc161858765d92def982c6405e2
as604443@gmail.com <adityasingh@Adityas-MacBook-Air.local>
2024-10-27 16:29:51 +0530

    datatypes in javascript

---
+++ datatypes.js
@@ -0,0 +1,10 @@
+let a = 15
+let b = "hello"
+let c = true
+let d = null
+let e = undefined
+console.table([typeof a, typeof b, typeof c, typeof d, typeof e])
+/*
+typeof null is object
+typeof undefined is undefined
+*/
