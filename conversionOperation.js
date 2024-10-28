d78e02467e02c202dc00b8975e05508975de5bc6
as604443@gmail.com <adityasingh@Adityas-MacBook-Air.local>
2024-10-28 22:43:25 +0530

    conversion operation in javascript

---
+++ conversionOperation.js
@@ -0,0 +1,21 @@
+let a = "88"
+let b = Number(a)
+console.log(b);
+console.log(typeof b);
+let x = "acb99"
+let y = Number(x)
+console.log(y);
+console.log(typeof y);
+let z = null
+let w = Number(z)
+console.log(w);
+console.log(typeof w);
+let m = undefined
+let n = Number(m)
+console.log(n);
+console.log(typeof n);
+let istrue = ""
+let isnumber = Boolean(istrue)
+console.log(isnumber);
+console.log(typeof isnumber);
+
