b537b355b384f82e81262d7f6721f9b486bf40a4
as604443@gmail.com <adityasingh@Adityas-MacBook-Air.local>
2024-10-28 23:16:58 +0530

    operations in javascript

---
+++ operation.js
@@ -0,0 +1,14 @@
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+// console.log(1 + "2" + 3);
+// console.log(1 + "2" + "3");
+// console.log(+true);
+// console.log(+false);
+// console.log(+null);
+// console.log(+undefined);
+// console.log(+"");
+// console.log(+"a");
+// console.log(+0);
+// console.log(+1);
+// console.log(+NaN);
+console.log(3+5*4%8/2);//*,/,% have the same precedence so associativity wiis left to right then 3 is added.
