3411e4270ff0f381ff40752f2acc6bd83af9d4e1
as604443@gmail.com <adityasingh@Adityas-MacBook-Air.local>
2024-10-30 20:59:35 +0530

    datatypes_summary in js

---
+++ datatypes_summary.js
@@ -0,0 +1,19 @@
+// let id = Symbol("123");
+// let id2 = Symbol("123");
+// console.log(typeof id);
+// console.log(typeof id2);
+// let bigNumber = 1234567899018081297391n;
+// console.log(typeof bigNumber);
+// let arr = [1, 2, 3, 4, 5];
+// console.log(typeof arr);//object
+// let obj = {
+//     name: "Aditya",
+//     age: 18,
+//     isMarried: false
+// }
+// // console.log(typeof obj);//object
+// let func = function () {
+//     console.log("hello world");
+// }
+// func();
+// console.log(typeof func);//function
