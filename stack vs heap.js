d0b25abda42990f61995d44b1b227ca7a51864c8
as604443@gmail.com <adityasingh@Adityas-MacBook-Air.local>
2024-10-30 21:19:26 +0530

    stack and heap concept in js

---
+++ stack vs heap.js
@@ -0,0 +1,20 @@
+//stack --> all primitive data types are stored in stack
+//heap --> all objects are stored in heap or non primitive data types
+// let name1 = "Aditya"
+// let name2 = name1
+// name2 = "Aditya Singh"
+// console.log(name1);
+// console.log(name2);
+
+/* heap
+
+// let employee1 = {
+//     name: "Aditya",
+//     age: 18
+// }
+// let employee2 = employee1
+// employee2.name = "Aditya Singh"
+// console.log(employee1.name);
+// console.log(employee2.name);
+
+*/
