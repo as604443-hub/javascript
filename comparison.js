7e3ec12f16153756165873ea56587ac4e5803ccb
as604443@gmail.com <adityasingh@Adityas-MacBook-Air.local>
2024-10-30 14:44:33 +0530

    comparison in js

---
+++ comparison.js
@@ -0,0 +1,42 @@
+// console.log(null > 0)
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null != 0);
+// console.log(null >= 0);
+// console.log(undefined >= 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined != 0);
+/* console.log(null == undefined); //both null and undefined are falsy values in javascript(after type coercion)
+console.log(null != undefined);
+ console.log(null === undefined); //in strictly equal type coercion is not happening so false
+console.log(null > undefined);//null specifies 0 and undefined specifies NaN so there is no comparison
+ console.log(null >= undefined);*/
+// console.log("2" > 1);
+// console.log("2" >= 1);
+// console.log("2" == 1);
+// console.log("2" !== 1);
+// console.log("2" === 1);
+// console.log("2" - 1);
+// console.log("2" - "1");
+// console.log("2" + "a");
+// console.log("2" + 1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
